1b2a2e4d7b52ed588821b66529608aec62291cc6
asmagala <asmagala@op.pl>
2020-05-26 15:24:40 +0200

    Init

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/contact.html'));
+});
+
+app.get('/info', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/info.html'));
+});
+
+app.get('/history', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/history.html'));
+});
+
+app.get('/sendFile', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pliki/aaa.pdf'));
+});
+
+
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
